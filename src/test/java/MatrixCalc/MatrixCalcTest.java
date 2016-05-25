7f4b4de28e6e67e79adaecc52574a55c6fb71321
samhareem <sami.harju@cs.helsinki.fi>
2016-05-25 19:33:59 +0300

    Added tests to base methods

--- src/test/java/MatrixCalc/MatrixCalcTest.java
+++ src/test/java/MatrixCalc/MatrixCalcTest.java
@@ -13,21 +13,29 @@ public class MatrixCalcTest {
     public final double[][] matrixA = {{1,2,3}, {4,5,6}, {6,7,8}};
 
     @Test
-    public void addingTrowsIllegalArgumentExceptionWithJaggedMatrix() {
+    public void addThrowsExceptionWithJaggedMatrix() {
         double[][] invalidMatrix = {{0,1,2}, {0,1,2,3}, {0,1,2}};
         exception.expect(IllegalArgumentException.class);
         MatrixCalc.add(matrixA, invalidMatrix);
     }
 
     @Test
-    public void addingEmptyMatrixThrowsIllegalArgumentException() {
+    public void addThrowsExceptionWithEmptyMatrix() {
         double[][] emptyMatrix = new double[0][0];
         exception.expect(IllegalArgumentException.class);
         MatrixCalc.add(emptyMatrix, emptyMatrix);
     }
 
     @Test
-    public void addingFunctionsProperly() {
+    public void addThrowsExceptionWithMatricesOfDifferentSize() {
+        double[][] sizeOneMatrix = {{1}};
+        double[][] sizeTwoMatrix = {{1,2}, {3,4}};
+        exception.expect(IllegalArgumentException.class);
+        MatrixCalc.add(sizeOneMatrix, sizeTwoMatrix);
+    }
+
+    @Test
+    public void addFunctionsProperly() {
         double[][] result = MatrixCalc.add(matrixA, matrixA);
         for (int y = 0; y < matrixA.length; y++) {
             for (int x = 0; x < matrixA[0].length; x++) {
@@ -36,5 +44,36 @@ public class MatrixCalcTest {
         }
     }
 
+    @Test
+    public void subtractThrowsExceptionWithJaggedMatrix() {
+        double[][] invalidMatrix = {{0,1,2}, {0,1,2,3}, {0,1,2}};
+        exception.expect(IllegalArgumentException.class);
+        MatrixCalc.subtract(matrixA, invalidMatrix);
+    }
+
+    @Test
+    public void subtractThrowsExceptionWithEmptyMatrix() {
+        double[][] emptyMatrix = new double[0][0];
+        exception.expect(IllegalArgumentException.class);
+        MatrixCalc.subtract(emptyMatrix, emptyMatrix);
+    }
+
+    @Test
+    public void subtractThrowsExceptionWithMatricesOfDifferentSize() {
+        double[][] sizeOneMatrix = {{1}};
+        double[][] sizeTwoMatrix = {{1,2}, {3,4}};
+        exception.expect(IllegalArgumentException.class);
+        MatrixCalc.add(sizeOneMatrix, sizeTwoMatrix);
+    }
+
+    @Test
+    public void subtractFunctionsProperly() {
+        double[][] result = MatrixCalc.subtract(matrixA, matrixA);
+        for (int y = 0; y < matrixA.length; y++) {
+            for (int x = 0; x < matrixA[0].length; x++) {
+                assertEquals(matrixA[y][x]-matrixA[y][x], result[y][x], 0.001);
+            }
+        }
+    }
 
 }
